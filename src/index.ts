c5543fd023bba96438de0df0a7214c84a69af23b
nabcellent <nabcellent.dev@gmail.com>
2022-04-10 17:37:16 +0300

    Initial Commit
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,11 @@ import { AppDataSource } from "./data-source";
 import log from './utils/logger';
 import App from './app';
 import validateEnv from './utils/validateEnv';
-import { SavingsAccount } from './entities/models/SavingsAccount';
 
 validateEnv();
 
 AppDataSource.initialize().then(async () => {
-    console.log("Loading accounts from the database...")
-    const accounts = await AppDataSource.manager.find(SavingsAccount)
-    console.log("Loaded accounts: ", accounts)
-
     const app = new App(Number(process.env.PORT || 8005));
+
     app.listen();
 }).catch(error => log.error('Database connection error: ', error))
