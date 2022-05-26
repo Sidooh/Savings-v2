e9374d1449ffec07366de4d0fe7b3ffea737e1be
nabcellent <nabcellent.dev@gmail.com>
2022-05-26 11:05:57 +0300

    Build Dockerfile & Deploy to GCP
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/http/middleware/error.middleware.ts
+++ src/http/middleware/error.middleware.ts
@@ -5,12 +5,8 @@ import { AxiosError } from 'axios';
 export const ErrorMiddleware = (error: Error | any, req: Request, res: Response, next: NextFunction) => {
     let message = error.message || 'Something went wrong';
 
-    if (error instanceof CustomError) {
-        return res.status(error.statusCode).send({errors: error.serializeErrors()});
-    }
-    if (error instanceof AxiosError) {
-        return res.status(error.response.status).send(error.response.data);
-    }
+    if (error instanceof CustomError) return res.status(error.statusCode).send({errors: error.serializeErrors()});
+    if (error instanceof AxiosError) return res.status(error.response.status).send(error.response.data);
 
     console.error(error);
 
