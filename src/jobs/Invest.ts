7344f8364e0ca356b71b4946130f6e8459bf7a8b
Nabcellent <nabcellent.dev@gmail.com>
2022-07-11 12:20:29 +0300

    Adds interest rate to env with a default of 9
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/jobs/Invest.ts
+++ src/jobs/Invest.ts
@@ -5,6 +5,5 @@ import log from '../utils/logger';
 export const Invest = () => {
     log.info("...[JOB]... Setting up Investment job...");
 
-    schedule('* * * * *', async () => await (new InvestmentRepository()).invest());
-    // schedule('0 0 * * *', async () => await (new InvestmentRepository()).invest());
+    schedule('0 0 * * *', async () => await (new InvestmentRepository()).invest());
 };
