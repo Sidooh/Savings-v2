cbc12cf0782b81aef33d569099ec678406565be7
Nabcellent <nabcellent.dev@gmail.com>
2022-07-20 09:39:18 +0300

    Formats code
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/repositories/TransactionRepository.ts
+++ src/repositories/TransactionRepository.ts
@@ -73,10 +73,10 @@ export const TransactionRepository = {
 
         log.info(`...Processing ${transactions.length} transactions`);
 
-        const results = {}
+        const results = {};
         for (const t of transactions) {
             try {
-                const account = await SidoohAccounts.find(t.personal_account.account_id)
+                const account = await SidoohAccounts.find(t.personal_account.account_id);
                 // Withdraw from account
                 // TODO: create method, helper to get withdrawal amount
 
@@ -86,26 +86,26 @@ export const TransactionRepository = {
                         .then(async res => {
 
                             if (res && res.status === Status.COMPLETED) {
-                                t.payment.status = Status.COMPLETED
-                                await t.payment.save()
+                                t.payment.status = Status.COMPLETED;
+                                await t.payment.save();
 
-                                t.status = Status.COMPLETED
-                                await t.save()
+                                t.status = Status.COMPLETED;
+                                await t.save();
 
-                                results[t.id] = t.payment
+                                results[t.id] = t.payment;
 
                                 // Notify user
                             }
 
                             if (res && res.status == Status.FAILED) {
-                                console.log({res})
-                                t.payment.status = Status.FAILED
-                                await t.payment.save()
+                                console.log({res});
+                                t.payment.status = Status.FAILED;
+                                await t.payment.save();
 
-                                t.status = Status.FAILED
-                                await t.save()
+                                t.status = Status.FAILED;
+                                await t.save();
 
-                                results[t.id] = t.payment
+                                results[t.id] = t.payment;
 
                                 // Notify user
                             }
@@ -113,20 +113,19 @@ export const TransactionRepository = {
                             // TODO: Handle for other status or non-existent (undefined res)
 
                         }, error => {
-                            throw new Error('Payment already initialized')
-                        })
-
+                            throw new Error('Payment already initialized');
+                        });
                 } else {
                     if (t.personal_account.balance - 50 < t.amount) {
-                        throw new Error('Account Balance is insufficient')
+                        throw new Error('Account Balance is insufficient');
                     }
 
-                    t.personal_account.balance -= t.amount
-                    await t.save()
+                    t.personal_account.balance -= t.amount;
+                    await t.save();
 
                     // Request Payment
-                    const description = t.description.split(' - ')
-                    const destination = description.length > 1 ? description[1] : account['phone']
+                    const description = t.description.split(' - ');
+                    const destination = description.length > 1 ? description[1] : account['phone'];
                     await SidoohPayments.requestPayment({
                         amount: t.amount,
                         destination,
@@ -135,18 +134,17 @@ export const TransactionRepository = {
                         account_id: account['id']
                     })
                         .then(async res => {
-                                const p = await Payment.save({
+                                results[t.id] = await Payment.save({
                                     type: TransactionType.DEBIT,
                                     description: Description.ACCOUNT_WITHDRAWAL,
                                     amount: t.amount,
                                     transaction: t,
                                     reference: res.id,
-                                })
-                                results[t.id] = p
+                                });
                             },
                             async error => {
-                                throw new Error("Failed")
-                            })
+                                throw new Error("Failed");
+                            });
 
                 }
 
@@ -154,13 +152,13 @@ export const TransactionRepository = {
                 // results[t.id] = payment
             } catch (e) {
                 //reverse actions // rollback
-                t.personal_account.balance += t.amount
-                await t.personal_account.save()
+                t.personal_account.balance += t.amount;
+                await t.personal_account.save();
 
-                results[t.id] = e.message
+                results[t.id] = e.message;
             }
 
-            SidoohProducts.withdrawalCallback({...t, personal_account: undefined, payment: undefined})
+            SidoohProducts.withdrawalCallback({...t, personal_account: undefined, payment: undefined});
         }
 
         log.info("...Processed Personal Withdrawals!", results);
@@ -168,38 +166,38 @@ export const TransactionRepository = {
 
 
     processPaymentCallback: async (data: any) => {
-        console.log('processPaymentCallback', {data})
+        console.log('processPaymentCallback', {data});
         const payment = await Payment.findOne({
             where: {
                 reference: data.id
             },
             relations: {transaction: true}
-        })
+        });
 
-        console.log({payment})
+        console.log({payment});
 
         if (data.status === Status.COMPLETED) {
-            payment.status = Status.COMPLETED
-            await payment.save()
+            payment.status = Status.COMPLETED;
+            await payment.save();
 
-            payment.transaction.status = Status.COMPLETED
-            await payment.transaction.save()
+            payment.transaction.status = Status.COMPLETED;
+            await payment.transaction.save();
 
             // Notify user
         }
 
         if (data.status == Status.FAILED) {
-            payment.status = Status.FAILED
-            await payment.save()
+            payment.status = Status.FAILED;
+            await payment.save();
 
-            payment.transaction.status = Status.FAILED
-            await payment.transaction.save()
+            payment.transaction.status = Status.FAILED;
+            await payment.transaction.save();
 
             // Notify user
         }
 
-        SidoohProducts.withdrawalCallback({...payment.transaction})
+        SidoohProducts.withdrawalCallback({...payment.transaction});
 
-        return {}
+        return {};
     }
 };
