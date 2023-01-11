7a272e5a3f47aaf55b65d631396c3031b02ed692
Nabcellent <nabcellent.dev@gmail.com>
2023-01-11 19:53:49 +0300

    Adds tests
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -25,26 +25,28 @@ class App {
 
         /** --------------------------------    INIT SENTRY
          * */
-        Sentry.init({
-            dsn: env.SENTRY_DSN,
-            integrations: [
-                // enable HTTP calls tracing
-                new Sentry.Integrations.Http({ tracing: true }),
-                // enable Express.js middleware tracing
-                new Tracing.Integrations.Express({ app: this.app }),
-            ],
+        if (env.NODE_ENV !== 'test') {
+            Sentry.init({
+                dsn: env.SENTRY_DSN,
+                integrations: [
+                    // enable HTTP calls tracing
+                    new Sentry.Integrations.Http({ tracing: true }),
+                    // enable Express.js middleware tracing
+                    new Tracing.Integrations.Express({ app: this.app }),
+                ],
 
-            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-            // We recommend adjusting this value in production
-            tracesSampleRate: env.SENTRY_TRACES_SAMPLE_RATE,
-        });
+                // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+                // We recommend adjusting this value in production
+                tracesSampleRate: env.SENTRY_TRACES_SAMPLE_RATE,
+            });
 
-        // RequestHandler creates a separate execution context using domains, so that every
-        // transaction/span/breadcrumb is attached to its own Hub instance
-        this.app.use(Sentry.Handlers.requestHandler());
-        // TracingHandler creates a trace for every incoming request
-        this.app.use(Sentry.Handlers.tracingHandler());
-        this.app.use(Sentry.Handlers.errorHandler());
+            // RequestHandler creates a separate execution context using domains, so that every
+            // transaction/span/breadcrumb is attached to its own Hub instance
+            this.app.use(Sentry.Handlers.requestHandler());
+            // TracingHandler creates a trace for every incoming request
+            this.app.use(Sentry.Handlers.tracingHandler());
+            this.app.use(Sentry.Handlers.errorHandler());
+        }
 
         /** --------------------------------    INIT APP MIDDLEWARE
          * */
