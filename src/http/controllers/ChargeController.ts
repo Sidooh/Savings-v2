5a4e3c4036a61d47c7169cf5d836f16f14ab3309
Nabcellent <nabcellent.dev@gmail.com>
2023-03-17 16:44:43 +0300

    Adds endpoint to get withdrawal charge.
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/http/controllers/ChargeController.ts
+++ src/http/controllers/ChargeController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import Controller from './Controller';
 import SidoohPayments from "../../services/SidoohPayments";
 
-export default class EarningController extends Controller {
+export default class ChargeController extends Controller {
     static getWithdrawalCharge = async ({ params, body }: Request, res: Response) => {
         const charge = await SidoohPayments.getWithdrawalCharge(body.amount)
 
